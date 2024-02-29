76d24f8028f40fa6e605a59a325d667af30a925f
JuanMullerP <126712162+JuanMullerP@users.noreply.github.com>
2024-02-29 15:58:12 -0300

    configurações iniciais concluidas

--- nextjs/next-crud/src/pages/api/hello.ts
+++ nextjs/next-crud/src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import {pool} from '../../config/db'
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  await pool.query(`insert into cliente(nome,idade,email,pai) values('maria3',12,'mariamaria@gcinto.com','alinelson')`)
+  let [resposta]= await pool.query('SELECT * FROM cliente')
+  console.log(resposta)
   res.status(200).json({ name: "John Doe" });
 }
